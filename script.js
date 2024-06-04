43ae5cf9e1ab908d9fdf080b4df6467865fc58e6
chessmano <144106704+chessmano@users.noreply.github.com>
2024-06-04 14:50:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,16 @@ function appendToDisplay(value) {
 function calculate() {
     var display = document.getElementById('display').value;
     try {
+        display = display.replace(/√\(/g, 'Math.sqrt(');
+        display = display.replace(/π/g, 'Math.PI');
+        display = display.replace(/e/g, 'Math.E');
+        display = display.replace(/log\(/g, 'Math.log10(');
+        display = display.replace(/ln\(/g, 'Math.log(');
+        display = display.replace(/exp\(/g, 'Math.exp(');
         display = display.replace(/sin\(/g, 'Math.sin(');
         display = display.replace(/cos\(/g, 'Math.cos(');
         display = display.replace(/tan\(/g, 'Math.tan(');
+        display = display.replace(/\^/g, '**');
         var result = eval(display);
         document.getElementById('display').value = result;
     } catch (e) {
