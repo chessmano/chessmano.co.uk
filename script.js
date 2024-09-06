a3c583598d30229422df102ecb9a2ec03a7463c2
chessmano <144106704+chessmano@users.noreply.github.com>
2024-09-06 12:11:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Set a timeout to redirect after 1 second (1000 milliseconds)
+// Set a timeout to redirect to nextpage.html after 1 second (1000 milliseconds)
 setTimeout(function() {
     window.location.href = 'nextpage.html';
 }, 1000); // 1000 milliseconds = 1 second
-
